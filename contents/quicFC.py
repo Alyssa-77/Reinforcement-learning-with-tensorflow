a3ce6f5c12452a069d436bb296afd9569d550eed
Alyssa-77 <105105888+Alyssa-77@users.noreply.github.com>
2022-05-10 13:18:01 +0800

    my quic RL
    still have problem

---
+++ contents/quicFC.py
@@ -0,0 +1,16 @@
+'''
+Using:
+python 3.7.4
+tensorflow 1.15
+'''
+
+import numpy as np
+import tensorflow as tf
+from RL_brain import PolicyGradient
+# import gym
+
+np.random.seed(2)
+tf.set_random_seed(2)  # reproducible 可重現的
+
+
+
