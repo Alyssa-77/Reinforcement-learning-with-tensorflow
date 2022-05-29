514186af7c6b650c74bdf950c3b6ba090b88a3c1
Alyssa-77 <105105888+Alyssa-77@users.noreply.github.com>
2022-05-29 16:26:26 +0800

    Update oneQ.py

--- contents/0_me/Qlearning/oneQ.py
+++ contents/0_me/Qlearning/oneQ.py
@@ -5,15 +5,12 @@ Run with client.py
 After set miniet(bw delay), 輸入f的index, 自動跑10次, 取得dbpo, 存在/myperf/zscore。
 f1 = [4000, 8000, 10000, 20000, 40000, 80000, 100000, 200000, 400000, 800000, 1000000] 16KB~16MB
 c1 = 16MB = 1000000
-
-改LOG、FLOW_SIZE、SHELL、rewriteWnd(string、changes)
 """
 
 import subprocess, os
 import json
 import socket
 import time
-import math
 
 # kill process ================================================================
 def kill_process(target):
@@ -41,15 +38,20 @@ def client_socket(host, port, outdata):
     s.close()
 
 def getData(data):
+    global ACTION
     data = eval(data)
     if data["type"] == "perf":      # get perf
         s = calState(data["item"][0])
-        print("get perf: bw=",s['b'],"loss=",s['o'],"delay=",s['d'],"tput=",s['p'])
-        if FIRST==False:
-            r = calReward(s)
+        print("state: bw=",s['b'],"loss=",s['o'],"delay=",s['d'],"tput=",s['p'])
+        if FIRST==True:
+            r = 0
+            a = "0x10000"
+        else:
+            r = calReward(s, ACTION)
+            print("reward:", r)
 
         # (0) save perf as state txt, for real zscore & check performance
-        state_txt = LOG+"state.txt"
+        state_txt = LOG
         appendTxt(state_txt, s['b'], s['o'], s['d'], s['p'])
 
         # (1) open another h1, close SHELL
@@ -57,7 +59,8 @@ def getData(data):
 
         # (3) RL cal a
         a = calAction(s, r)
-        print("choose wnd =",a)
+        ACTION = a
+        print("choose wnd:",a)
 
         # (4) change SHELL wnd size
         rewriteWnd(STREAM[0], a)   # shell
@@ -72,15 +75,16 @@ def calState(perfDict): #cal from link status
     state = {'b':bw, 'o':loss, 'd':delay, 'p':tput}     #todo 一開始沒收到職怎辦
     return state
 
-def calAction(s):
+def calAction(s,r):
     # print(wnd)
     wnd="0x20000" # todo
     return wnd
 
 def calReward(s, a):
     # print("reward")
-    z = zscore_fix(s['o'], s['d'], s['p'], wnd=a) # z=[zo, zd, zp]
-    r = W_LOSS*z[0]+W_DELAY*z[1]+W_TPUT*z[2]   # (-0.5loss, -0.5delay, 0tput) # todo reward設計
+    z = zscore_fix(s['o'], s['d'], s['p'], wnd=a) # z=[zo, zd, zp] wnd="0x10000"  todo 每輪更新
+    print("z=",z[0],z[1],z[2])
+    r = round(W_LOSS*z[0]+W_DELAY*z[1]+W_TPUT*z[2], 3)   # (-0.5loss, -0.5delay, 0tput)
     return r
 
 # other function ================================================================
@@ -113,7 +117,7 @@ def resetTxt(txt):
     f.write("bw (Mbps),loss (%),delay (ms),tput (kbps)\n")
     f.close()
 
-def zscore_fix(o, d, p, wnd): # 固定值下去算，todo 改每輪變化
+def zscore_fix(o, d, p, wnd): # 固定值下去算，todo 每輪更新
     avg = [ # avg(o,d,p) (100M, 10^6)
     [0.15, 128.34, 63172.2], [0.66, 141.57, 57546.1], [0.04, 155.57, 59342.0], 
     [0.12, 147.32, 56566.3], [0.49, 123.78, 65289.5], [1.02, 120.21, 66622.1], [0.01, 115.79, 69234.6], 
@@ -124,9 +128,9 @@ def zscore_fix(o, d, p, wnd): # 固定值下去算，todo 改每輪變化
     [1.81, 5.72, 3475.82], [1.82, 4.99, 2709.52], [1.82, 41.25, 11041.77], [1.81, 22.07, 8354.89]]
     
     wnd_index = get_key(F_DICT, wnd)
-    zd = (d-avg[wnd_index][0])/sd[wnd_index][0] # Z=(x-avg)/sd
-    zp = (p-avg[wnd_index][1])/sd[wnd_index][1]
-    zo = (o-avg[wnd_index][2])/sd[wnd_index][2]
+    zo = (o-avg[wnd_index][0])/sd[wnd_index][0] # Z=(x-avg)/sd
+    zd = (d-avg[wnd_index][1])/sd[wnd_index][1]
+    zp = (p-avg[wnd_index][2])/sd[wnd_index][2]
     return [zo, zd, zp]
 
 def get_key(my_dict, val):
@@ -136,10 +140,10 @@ def get_key(my_dict, val):
     return "key doesn't exist"
 
 # Main function =============================================================
-EP = 2         # RL run ep times
+EP = 3         # RL run ep times
 MAKE_SEC = 16   # C make second
 
-LOG = "/home/yuki/myquic/msquic/myperf/oneQ/"
+LOG = "/home/yuki/myquic/msquic/myperf/oneQ/state.txt"
 F_DICT = {0:"0x4000", 1:"0x8000", 2:"0x10000", 3:"0x20000", 4:"0x40000", 5:"0x80000", 6:"0x100000", 7:"0x200000", 8:"0x400000", 9:"0x800000", 10:"0x1000000"}
 
 STREAM = ["/home/yuki/myquic/msquic/myshell/server_stream.sh", "server_stream"] # shell, kill
@@ -149,18 +153,21 @@ W_LOSS = -0.5    # loss
 W_DELAY = -0.5   # delay
 W_TPUT = 0    # tput
 
-
-FIRST=True
+ACTION = "0x10000"
+FIRST = True
 for i in range(EP): # todo while true or not done
     if i==0:
         print("reset stream window = 0x10000")
         rewriteWnd(STREAM[0], "0x10000")
+        resetTxt(LOG)   # reset state.txt
     else:
         FIRST=False
 
     subprocess.Popen(["sudo", "xterm", "-e", STREAM[0]]) 
-    print("\nwait for make:", MAKE_SEC,"s")
+    print("\nep:",i+1,", wait for make:", MAKE_SEC,"s")
     for j in range(MAKE_SEC):
         time.sleep(1)
     sendData = {"type" : "flag"} # send flag to run client
     client_socket('10.0.0.2', 7002, sendData)
+
+print("\n==== done ====\nstate save at"+LOG)
